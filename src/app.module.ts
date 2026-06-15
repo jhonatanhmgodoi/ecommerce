ebc91386244394b4c574b21d0b1adef364f082d9
jhonatan godoi <jhonatan@contaja.com.br>
2026-06-15 06:20:42 -0300

    feat: add global configuration and throttling module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1 * 100 * 600, // 1 minuto
+        limit: 100 // 100 reques por minuto
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
