69b2faed2e76c0106c36aced57858adf6feaf858
jhonatan godoi <jhonatan@contaja.com.br>
2026-06-15 16:42:54 -0300

    feat: create gateway config

---
+++ src/config/gateway.config.ts
@@ -0,0 +1,18 @@
+export const serviceCondig = {
+  users: {
+    url: process.env.USER_SERVICE_URL || 'http://localhost:3000',
+    timeout: 10000,
+  },
+  products: {
+    url: process.env.PRODUCTS_SERVICE_URL || 'http://localhost:3001',
+    timeout: 10000,
+  },
+  checkout: {
+    url: process.env.CHECKOUT_SERVICE_URL || 'http://localhost:3000',
+    timeout: 10000,
+  },
+  payments: {
+    url: process.env.PAYMENTS_SERVICE_URL || 'http://localhost:3000',
+    timeout: 10000,
+  },
+} as const;
