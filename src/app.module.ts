235a0393a8f560acfca986c9ef02649b1584ae2b
jhonatan godoi <jhonatan@contaja.com.br>
2026-06-17 11:12:30 -0300

    chore: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ProxyModule } from './proxy/proxy.module';
+import { MiddlewareModule } from './middleware/middleware.module';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
 @Module({
   imports: [
@@ -17,6 +19,7 @@ import { ProxyModule } from './proxy/proxy.module';
       },
     ]),
     ProxyModule,
+    MiddlewareModule,
   ],
   controllers: [AppController],
   providers: [AppService],
